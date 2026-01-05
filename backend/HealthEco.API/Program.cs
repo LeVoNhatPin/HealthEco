95b9b24a3abbac6cbd3558882faea99e6dc3a942
LeVoNhatPin <pinle2409@gmail.com>
2026-01-05 15:17:59 +0700

    fix program

--- backend/HealthEco.API/Program.cs
+++ backend/HealthEco.API/Program.cs
@@ -27,11 +27,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
