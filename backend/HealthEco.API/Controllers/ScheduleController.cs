c8977b3d83c5c183d3dc34afbe5a372224986709
LeVoNhatPin <pinle2409@gmail.com>
2026-01-22 18:59:33 +0700

    datlich 35

--- backend/HealthEco.API/Controllers/ScheduleController.cs
+++ backend/HealthEco.API/Controllers/ScheduleController.cs
@@ -10,7 +10,7 @@ namespace HealthEco.API.Controllers
 {
     [ApiController]
     [Route("api/v1/schedule")]
-    [AllowAnonymous]
+    [Authorize(Roles = "Doctor")]
     public class ScheduleController : BaseController
     {
         private readonly ApplicationDbContext _context;
