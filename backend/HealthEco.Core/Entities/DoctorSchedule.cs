dcc7ae8379b223de5b6f163655ed389cbab3dfa5
LeVoNhatPin <pinle2409@gmail.com>
2026-01-22 23:56:34 +0700

    fix datlich 49

--- backend/HealthEco.Core/Entities/DoctorSchedule.cs
+++ backend/HealthEco.Core/Entities/DoctorSchedule.cs
@@ -12,17 +12,32 @@ namespace HealthEco.Core.Entities
         // public int FacilityId { get; set; }
         // public MedicalFacility? Facility { get; set; }
 
+        [Column("dateofweek")]
+
         public int DayOfWeek { get; set; } // 0-6
 
+        [Column("starttime")]
+
         public TimeSpan StartTime { get; set; }
+
+        [Column("endtime")]
+
         public TimeSpan EndTime { get; set; }
 
+        [Column("slotduration")]
+
         public int SlotDuration { get; set; } = 30;
+        [Column("maxpatientsperslot")]
+
         public int MaxPatientsPerSlot { get; set; } = 1;
+        [Column("isactive")]
 
         public bool IsActive { get; set; } = true;
+        [Column("validfrom")]
 
         public DateTime ValidFrom { get; set; }
+        [Column("validto")]
+
         public DateTime? ValidTo { get; set; }
 
         [Column("createdat")]
